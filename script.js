4279d83fd7711acc02943e6c78901751f553ed58
Yuliia00 <skaskevich2000@gmail.com>
2023-03-08 18:47:53 +0100

    hw_18

--- script.js
+++ script.js
@@ -10,9 +10,7 @@ let pow = (num, degree) => {
     if (degree === 0) {
         return 1;
     }
-    else {
-        return num * pow(num, degree - 1);
-    }
+    return num * pow(num, degree - 1);
 
 }
 alert(pow(num, degree));

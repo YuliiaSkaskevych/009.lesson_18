cbaff2f0221e5eaa6a0cb394975247a0f021f073
Yuliia00 <skaskevich2000@gmail.com>
2023-03-08 18:56:05 +0100

    hw_18_final_version

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let num = +prompt('Enter your number:');
 let degree = +prompt('Enter degree of your number:');
 let pow = (num, degree) => {
-    if (degree < 0) {
-        return alert(`You can't enter ${degree}! Use only natural numbers`);
+    if (degree <= 0) {
+        return 1;
     }
     if (degree === 1) {
         return num;
-    }
-    if (degree === 0) {
-        return 1;
     }
     return num * pow(num, degree - 1);
 
